cba92c86aae0cd0bc281df7f530d3f4cc7a85be1
Baerbel Hanle <hanlebaer@yahoo.de>
2017-07-06 16:02:02 +0200

    Farben

--- Calculations.java
+++ Calculations.java
@@ -69,6 +69,10 @@ public class Calculations {
     public double getBe() {
         return be;
     }
+    
+    public int getDepth(){
+        return depth;
+    }
 
     /**
      * Setter for aa
--- Graphics.java
+++ Graphics.java
@@ -42,13 +42,14 @@ public class Graphics {
         this.canvas = canvas;
         graphicsContext = canvas.getGraphicsContext2D();
         this.calculations = calculations;
-        setColors();
+        //setColors(calculations.getDepth());
     }
 
     /**
      * "Constructor" for the colors, in which Mandelbrot-set will be drawn.
      * Each array-item represents an iteration depth of Mandelbrot-set (modulo).
      */
+    /*
     private void setColors() {
         colors = new Color[23];
 
@@ -77,6 +78,102 @@ public class Graphics {
         colors[22] = INDIGO;
 
     }
+    */
+    
+    private void setColors(int depth){
+        
+        colors = new Color[depth];
+        
+        int b1 = depth/15;
+        int b2 = depth/2;
+        
+        
+        for(int i = 0; i < b1; i++){
+            colors[i] = colorGradient(i, 0, b1, 0);
+        }
+        
+        for(int i = b1; i < b2; i++){
+            colors[i] = colorGradient(i, b1, b2, 1);
+        }
+        
+        for(int i = b2; i < depth; i++){
+            colors[i] = colorGradient(i, b2, depth, 2);
+        }
+        
+        /*
+        double rf;
+        double gf;
+        double bf; 
+            
+        int r;
+        int g;
+        int b;
+            
+        for(int i = 0; i < depth/3; i++){
+            
+            rf = (double)i / ((double)depth/3);
+            gf = 1-rf;
+            bf = 0; 
+            
+            r = (int)(255*rf);
+            g = (int)(255*gf);
+            b = (int)(255*bf);
+                       
+            colors[i] = Color.rgb(r,g,b);
+        }
+        
+        for(int i = 0; i < depth/3; i++){
+            
+            gf = (double)i / ((double)depth/3);
+            bf = 1-gf;
+            rf = 0; 
+            
+            r = (int)(255*rf);
+            g = (int)(255*gf);
+            b = (int)(255*bf);
+                       
+            colors[i + depth/3] = Color.rgb(r,g,b);
+        }
+        
+        for(int i = 0; i < depth/3; i++){
+            
+            bf = (double)i / ((double)depth/3);
+            rf = 1-bf;
+            gf = 0; 
+            
+            r = (int)(255*rf);
+            g = (int)(255*gf);
+            b = (int)(255*bf);
+                       
+            colors[i + 2*depth/3] = Color.rgb(r,g,b);
+        }
+        
+        if(depth > 2)colors[depth-2] = Color.rgb(1,1,1);
+        if(depth > 2)colors[depth-1] = Color.rgb(1,1,1);
+        */
+    }
+    
+    private Color colorGradient(int i, int lower, int higher, int bereich){
+        
+        Color result;
+        
+        double ratio1 = (double)(i-lower)/(double)(higher - lower);
+        double ratio2 = 1 - ratio1;
+      
+        int c1 = (int)(255*ratio1);
+        int c2 = (int)(255*ratio2);
+        
+        if(bereich == 0){
+            return (Color.rgb(c2, c2-c2/4, c2));
+        }else if(bereich == 1){
+            return (Color.rgb(c2, c2-c2/4, c2));
+        }else if(bereich == 2){
+            return (Color.rgb(c2, c2-c2/4, c2));
+        } else {
+            return (Color.rgb(1,1,1));
+        }
+        
+    }
 
     /**
      * Setter for Calculations
@@ -93,7 +190,7 @@ public class Graphics {
      */
     public void draw() {
 
-        setColors();
+        setColors(calculations.getDepth());
 
         double aa = calculations.getAa();
         double ba = calculations.getBa();
